f578450dda86af883c3bc72524f991cc579010d6
Teodora <150371708+sarcasticcatx@users.noreply.github.com>
2023-12-07 23:42:28 +0100

    Update zadaci.js

--- Homework#3/zadaci.js
+++ Homework#3/zadaci.js
@@ -9,3 +9,17 @@ function calculateAge(birthYear, currentYear) {
 calculateAge(2000);
 calculateAge(1997);
 calculateAge(1969);
+
+// Homework #1
+function differentTypes(userInput) {
+  return typeof userInput;
+}
+
+let person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+let neOdredena;
+
+console.log(differentTypes("kk"));
+console.log(differentTypes(person));
+console.log(differentTypes(555));
+console.log(differentTypes(true));
+console.log(differentTypes(neOdredena));
