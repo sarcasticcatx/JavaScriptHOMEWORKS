9d0e22f57857b94d0ac5f21c50df9e20ce39da98
Teodora <150371708+sarcasticcatx@users.noreply.github.com>
2023-12-19 01:32:49 +0100

    Update zadaci.js

--- Homework#4/zadaci.js
+++ Homework#4/zadaci.js
@@ -3,10 +3,12 @@ Homework #1
 function tellStory(stories) {
   let [ime, mood, activity] = stories;
   return `This is ${ime}. ${ime} is a nice person. Today they are
-${mood}. They are ${activity} all day. The end`;
+${mood}. They are ${activity} all day. The end.`;
 }
 let stories = ["Elka", "happy", "playing The Witcher 3 - Wild Hunt"];
 let story = tellStory(stories);
 console.log(story);
+//alert(story);
 // poradi nekoja prichina koga kje pisham name avtomatski mi go preshkrtuva
 
+Homework #2
