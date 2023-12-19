610f79d96d279dc868bd631766cc73adf348e076
Teodora <150371708+sarcasticcatx@users.noreply.github.com>
2023-12-19 01:31:16 +0100

    Update zadaci.js

--- Homework#4/zadaci.js
+++ Homework#4/zadaci.js
@@ -1,1 +1,12 @@
+Homework #1
+
+function tellStory(stories) {
+  let [ime, mood, activity] = stories;
+  return `This is ${ime}. ${ime} is a nice person. Today they are
+${mood}. They are ${activity} all day. The end`;
+}
+let stories = ["Elka", "happy", "playing The Witcher 3 - Wild Hunt"];
+let story = tellStory(stories);
+console.log(story);
+// poradi nekoja prichina koga kje pisham name avtomatski mi go preshkrtuva
 
